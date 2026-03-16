d9c279b4deb9177cc60e9692ef6ade1e513bcab5
mircavad09 <mircavad09@gmail.com>
2026-03-16 05:09:47 +0400

    Update app.js

--- app.js
+++ app.js
@@ -105,7 +105,7 @@ const DATA = {
   currency: "₼",
   plans: [
     { months: 1, label: "1 aylıq", price: 4.80 },
-    { months: 3, label: "3 aylıq", price: stokta yoxdur },
+    { months: 3, label: "stokta yoxdur", price: 0 },
   ],
 },
 
