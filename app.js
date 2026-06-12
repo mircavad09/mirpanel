880ac51bdebd82d2eb98125b07a5309c1312a65e
mircavad09 <mircavad09@gmail.com>
2026-06-12 13:04:39 +0400

    Update Mirpanel content from admin panel

--- app.js
+++ app.js
@@ -461,7 +461,7 @@ const DATA = {
       "flow": "email",
       "soldOut": false,
       "active": true,
-      "stock": 9,
+      "stock": 12,
       "stockEnabled": false,
       "seller": "",
       "bestSeller": false,
