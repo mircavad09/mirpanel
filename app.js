261f0446129d51d754341ce4bc2ed2e83ee2db48
mircavad09 <mircavad09@gmail.com>
2026-06-28 16:22:09 +0400

    Decrement stock for capcut

--- app.js
+++ app.js
@@ -137,7 +137,7 @@ const DATA = {
       "flow": "whatsapp",
       "soldOut": false,
       "active": true,
-      "stock": 43,
+      "stock": 42,
       "stockEnabled": true,
       "seller": "",
       "bestSeller": true,
