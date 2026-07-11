ebdf298be559b0fc4333857fe241ba1807dbe163
mircavad09 <mircavad09@gmail.com>
2026-07-11 15:55:31 +0400

    Use site page renderer for terms route

--- functions/sertler/[[path]].js
+++ functions/sertler/[[path]].js
@@ -1,6 +1,5 @@
+import { renderSitePage } from "../_site-page.js";
+
 export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  url.pathname = "/index.html";
-  url.search = "";
-  return context.env.ASSETS.fetch(new Request(url, context.request));
+  return renderSitePage(context, "sertler");
 }
